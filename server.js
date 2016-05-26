a94e3ef6a4443ed336eb5553de5aa8010531c4e7
gdalbelo <gdalbelo@vayon.com.br>
2016-05-26 15:17:31 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var express 	= require('express');
+var app 		= express();
+var path 		= require('path');
+var adminRotas 	= express.Router();
+
+adminRotas.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+adminRotas.get('/caio', function(req, res) {
+	res.sendFile(path.join(__dirname + '/caio.html'));
+});
+
+adminRotas.get('/clara', function(req, res) {
+	res.sendFile(path.join(__dirname + '/clara.html'));
+});
+
+adminRotas.get('/gabriel', function(req, res) {
+	res.sendFile(path.join(__dirname + '/gabriel.html'));
+});
+app.use('/fotos', adminRotas);
+
+app.listen(1337);
+console.log('1337 is the magic port!');
+
+
+
+
