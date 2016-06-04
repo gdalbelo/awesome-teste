e8a091e8a7fb29f0d64cd8465061dd854a6d4926
gdalbelo <gdalbelo@vayon.com.br>
2016-06-04 16:16:04 -0300

    Commit

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express 	= require('express');
 var app 		= express();
 var path 		= require('path');
-var adminRotas 	= express.Router();
+var adminRotas  = express.Router();
+var mainPage      = express.Router();
 
-adminRotas.get('/', function(req, res) {
+mainPage.get('/', function(req, res){
+  res.sendFile(path.join(__dirname + '/main.html'));
+})
+
+mainPage.get('/fotos', function(req, res) {
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
@@ -18,11 +23,13 @@ adminRotas.get('/clara', function(req, res) {
 adminRotas.get('/gabriel', function(req, res) {
 	res.sendFile(path.join(__dirname + '/gabriel.html'));
 });
+
+adminRotas.get('/miguel', function(req, res) {
+  res.sendFile(path.join(__dirname + '/miguel.html'));
+});
+
 app.use('/fotos', adminRotas);
+app.use('/', mainPage);
 
 app.listen(process.env.PORT || 1337);
 console.log('1337 is the magic port!');
-
-
-
-
